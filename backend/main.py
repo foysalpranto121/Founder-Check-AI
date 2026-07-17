f2ef62e0ecdc977482d6baf43343ea3c7a1901f5
foysalpranto121 <foysalpranto2002@gmail.com>
2026-07-17 23:46:45 +0600

    added  new file

--- backend/main.py
+++ backend/main.py
@@ -49,6 +49,10 @@ FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 origins = [
     FRONTEND_URL,
     FRONTEND_URL.replace("localhost", "127.0.0.1"),
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
 ]
 
 app.add_middleware(
